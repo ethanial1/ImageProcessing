1c55168a5a4e27d920aebc2801d3b8384d3dcfb7
ethanial1 <camiloeiz00@gmail.com>
2021-06-28 16:33:59 -0500

    Escala de grisises de una imagen

--- src/Image.java
+++ src/Image.java
@@ -22,5 +22,54 @@ public class Image {
     ImageInputStream imagen1;
     
     BufferedImage image;
+
+    // Método para obtener la matriz de la imagen
+    public void matrizDatos(){
+        try{
+            // Creamos el objeto input, contiene la ruta de la imagen
+            input = new FileInputStream("/Users/ethan/NetBeansProjects/BaseProgram/src/baseprogram/car.jpg");
+            imagen1 = ImageIO.createImageInputStream(input); // objeto que abre el archivo
+            
+            image = ImageIO.read(imagen1); // La podremos manipular
+            
+            //Obtenemos el número de filas de la imagen
+            fila = image.getWidth(); // lo ancho
+            //Obtenemos el número de columnas de la imagen
+            columna = image.getHeight(); // lo alto
+            
+            // inicializamos las matrices
+            ima = new int[fila][columna];
+            auxi = new int[fila][columna]; // matriz sobre la que se trabaja.
+            
+            System.err.println(fila);
+            System.err.println(columna);
+            
+            // Llamamos al método Escala de grisies
+            escalaGris();
+            
+        }catch(IOException e){
+            System.err.println(e);
+        }
+    }
+
+    // Aplicar una escala de gris
+    public void escalaGris(){
+        // ciclos for para recorrer la matriz
+        // Escala de grises
+        for (int y = 0; y < fila; y++) {
+            for (int x = 0; x < columna; x++) {
+                // 
+                int pixel = image.getRGB(y, x);
+                color = new Color(pixel); // regresa un color mediante un valor
+                    
+                // generamos/llenamos la matriz
+                ima[y][x] = (color.getRed() + color.getGreen() + color.getBlue()) / 3;
+            }
+        }
+        
+        // Guardamos la imagen
+        //guardarImagen(ima, "escalaGris");
+    }
+
     
 }
